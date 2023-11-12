b01283b28c4a26c61f010189cee1d55f2926cb0c
Maxwell <lubalecliff@gmail.com>
2023-11-12 20:17:31 +0300

    Completed assignment

--- src/components/TransactionTable.js
+++ src/components/TransactionTable.js
@@ -1,44 +1,41 @@
 import React, { useState } from 'react';
 
-const TransactionTable = ({ transactions, onDelete, onSort }) => {
-  const [sortKey, setSortKey] = useState(null);
+const TransactionTable = ({ transactions, onDelete }) => {
+  const [sortBy, setSortBy] = useState(null);
 
-  const handleSort = (key) => {
-    setSortKey(key);
-    onSort(key);
-  };
-
-  const handleDelete = (id) => {
-    onDelete(id);
-  };
-
-  const sortedTransactions = [...transactions].sort((a, b) => {
-    if (sortKey) {
-      return a[sortKey].localeCompare(b[sortKey]);
+  const handleSort = (property) => {
+    if (sortBy === property) {
+      // If already sorted by the same property, reverse the order
+      transactions.reverse();
+    } else {
+      // Sort the transactions by the selected property
+      transactions.sort((a, b) => (a[property] > b[property] ? 1 : -1));
     }
-    return 0;
-  });
+    setSortBy(property);
+  };
 
   return (
     <table>
       <thead>
         <tr>
-          <th onClick={() => handleSort('id')}>ID</th>
-          <th onClick={() => handleSort('description')}>Description</th>
-          <th onClick={() => handleSort('amount')}>Amount</th>
+          <th>
+            <button onClick={() => handleSort('description')}>Description</button>
+          </th>
+          <th>
+            <button onClick={() => handleSort('category')}>Category</button>
+          </th>
+          <th>Amount</th>
           <th>Action</th>
         </tr>
       </thead>
       <tbody>
-        {sortedTransactions.map((transaction) => (
+        {transactions.map((transaction) => (
           <tr key={transaction.id}>
-            <td>{transaction.id}</td>
             <td>{transaction.description}</td>
-            <td>${Number(transaction.amount).toFixed(2)}</td>
+            <td>{transaction.category}</td>
+            <td>{transaction.amount}</td>
             <td>
-              <button onClick={() => handleDelete(transaction.id)}>
-                Delete
-              </button>
+              <button onClick={() => onDelete(transaction.id)}>Delete</button>
             </td>
           </tr>
         ))}
